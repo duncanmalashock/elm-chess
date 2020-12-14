ed112465ca2323f41b296db00d6869391f63dcbf
Duncan Malashock <duncanmalashock@gmail.com>
2020-12-14 11:26:47 -0500

    Initial commit

---
+++ tests/Example.elm
@@ -0,0 +1,24 @@
+module Example exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    skip <|
+    describe ""
+        [ test "" <|
+            \_ ->
+                let
+
+
+                    expected =
+                        1
+
+                    result =
+                        2
+                in
+                Expect.equal expected result
+        ]
